57193a545d4d2975072e7004451acb24eeedfb0f
bellidin <nardineduc@gmail.com>
2025-04-29 08:55:35 -0300

    update

--- gerar_senha.php
+++ gerar_senha.php
@@ -4,5 +4,5 @@ $hash_admin = password_hash($senha, PASSWORD_DEFAULT);
 $hash_usuario = password_hash($senha, PASSWORD_DEFAULT);
 
 echo "Hash para admin: " . $hash_admin . "<br>";
-echo "Hash para usuario: " . $hash_usuario . "<n>";
+echo "Hash para usuario: " . $hash_usuario . "<br>";
 echo $senha;
